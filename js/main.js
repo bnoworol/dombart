4944086a2e97fef4642e6b6b0490fea4f855851a
Bartosz <noworolbartosz@gmail.com>
2023-05-08 09:40:13 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const allNavItems = document.querySelectorAll('.nav__link')
+const body = document.getElementById('body')
+const allNavItemsClose = document.querySelectorAll('.nav')
+const footerYear = document.querySelector('.footer__year')
+const navDesktop = document.querySelector('.nav__color-logo')
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+	body.classList.toggle('disable')
+	
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active')
+			navBtn.classList.remove('is-active')
+			body.classList.remove('disable')
+		})
+	})
+	allNavItemsClose.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active')
+			navBtn.classList.remove('is-active')
+			body.classList.remove('disable')
+		})
+	})
+}
+
+
+function addedBackground () {
+	const navbarY = navDesktop.offsetTop
+	if ( navbarY >= 280) {
+		navDesktop.style.backgroundColor = 'rgba(0,0,0, .3)'
+	} else {
+		navDesktop.style.backgroundColor = 'transparent'
+	}
+}
+
+
+
+
+
+
+
+[navBtn, allNavItemsClose].map(allNavItemsClose => navBtn.addEventListener('click', handleNav))
+
+
+function handleCurrentYear() {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
+document.addEventListener('scroll', addedBackground)
+
+
+
+
