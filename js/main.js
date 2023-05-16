9aba36653c5a8a0bf1548f7d75279e9a8e5389ba
Bartosz <noworolbartosz@gmail.com>
2023-05-16 10:04:36 +0200

    first commit - contact section

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ const body = document.getElementById('body')
 const allNavItemsClose = document.querySelectorAll('.nav')
 const footerYear = document.querySelector('.footer__year')
 const navDesktop = document.querySelector('.nav__color-logo')
-const arrow = document.querySelector('.header__arrow')
-const html = document.querySelector('html')
+const arrowUp = document.querySelector('.go-up')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
@@ -29,16 +28,15 @@ const handleNav = () => {
 	})
 }
 
-arrow.addEventListener('click', () => {
-	html.style.scrollPaddingTop = '140' + 'px'
-})
 
 function addedBackground() {
 	const navbarY = window.pageYOffset
 	if (navbarY >= 200) {
 		navDesktop.style.backgroundColor = '#f5f5f5'
+		arrowUp.style.visibility = 'visible'
 	} else {
 		navDesktop.style.backgroundColor = 'transparent'
+		arrowUp.style.visibility = 'hidden'
 	}
 }
 
@@ -50,3 +48,4 @@ function handleCurrentYear() {
 }
 handleCurrentYear()
 document.addEventListener('scroll', addedBackground)
+
