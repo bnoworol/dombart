e9481eee7edd96f174f098714018fe2d74ddc3ef
Bartosz <noworolbartosz@gmail.com>
2023-05-26 13:33:01 +0200

    added img in offers section

--- js/TwoProducts.js
+++ js/TwoProducts.js
@@ -1,19 +1,17 @@
 // KUBKI
 const imgLeftFirst = document.querySelectorAll('.img-left > img.cups_first')
 const imgLeftSecond = document.querySelectorAll('.img-left > img.cups_second')
-const imgLeftThird = document.querySelectorAll('.img-left > img.cups_third')
 const imgCenterFirst = document.querySelector('.img-center > img.cups_first')
 const imgCenterSecond = document.querySelector('.img-center > img.cups_second')
-const imgCenterThird = document.querySelector('.img-center > img.cups_third')
 
 const boxFirst = document.querySelector('.box__first')
 const boxSecond = document.querySelector('.box__second')
-const boxThird = document.querySelector('.box__third')
 const btnFirst = document.querySelector('.one__btn')
 const btnSecond = document.querySelector('.second__btn')
-const btnThird = document.querySelector('.third__btn')
+
 
 const html = document.querySelector('html')
+const view = document.querySelector('.view')
 // KUBKI
 function first(e) {
 	imgLeftFirst.forEach(el => {
@@ -42,39 +40,22 @@ function second(e) {
 imgLeftSecond.forEach(el => {
 	el.addEventListener('click', second)
 })
-function third(e) {
-	imgLeftThird.forEach(el => {
-		el.classList.remove('active')
-		function chooseImg(e) {
-			imgCenterThird.src = e.target.src
-			e.target.classList.add('active')
-		}
-		chooseImg(e)
-	})
-}
-imgLeftThird.forEach(el => {
-	el.addEventListener('click', third)
-})
 // 
+
 function ShowProducts1() {
 	html.style.scrollPaddingTop = '140' + 'px'
 	boxFirst.style.display = 'flex'
 	boxSecond.style.display = 'none'
-	boxThird.style.display = 'none'
+	view.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
 }
 function ShowProducts2() {
 	html.style.scrollPaddingTop = '140' + 'px'
 	boxFirst.style.display = 'none'
 	boxSecond.style.display = 'flex'
-	boxThird.style.display = 'none'
+	view.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
 }
-function ShowProducts3() {
-	html.style.scrollPaddingTop = '140' + 'px'
-	boxFirst.style.display = 'none'
-	boxSecond.style.display = 'none'
-	boxThird.style.display = 'flex'
-}
+
 
 btnFirst.addEventListener('click', ShowProducts1)
 btnSecond.addEventListener('click', ShowProducts2)
-btnThird.addEventListener('click', ShowProducts3)
+
